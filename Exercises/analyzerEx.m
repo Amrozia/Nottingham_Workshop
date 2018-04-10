4c6df75b4d52ebeafba32ca4a408507b0b6a527b
Amrozia <Amrozia@10.154.16.192>
2018-04-10 16:25:00 +0100

    Modified on course

--- Exercises/analyzerEx.m
+++ Exercises/analyzerEx.m
@@ -1,16 +1,16 @@
-function analyzer_ex
+function analyzerEx
 
 % Generate a 100-by-100 matrix where A(i,j) = i + j;
 for ii = 1:100
     for jj = 1:100
-        A(ii,jj) = ii + jj
+        A(ii,jj) = ii + jj;
     end
     end
     
 % Calculate the percentage of elements that are odd
 num_pos = sum(rem(A(:), 2) == 1);
-num_elements = prod(size(A);
-ratio_odd = num_pos/num_elemetns;
+num_elements = prod(size(A));
+ratio_odd = num_pos./num_elements;
 
 % Create an array of elements in backwards order
 backward_elements = fliplr(sort(A(:)));
